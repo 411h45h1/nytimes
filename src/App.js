7e3b450a2e8e7c3fe3c3124e5f28da3dbb8bca91
Ahmed <ahmedali.96@hotmail.com>
2020-08-22 20:36:22 -0400

    removed template data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const onMostpopular = async () => {
+    let req = await fetch(
+      "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=zE0uXeFH19AB52dh61Pf3gcbxOgciZxS"
+    );
+    let res = req.json();
+    console.log("res", res);
+  };
+  const onTopStories = async () => {
+    let req = await fetch(
+      "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=zE0uXeFH19AB52dh61Pf3gcbxOgciZxS"
+    );
+    let res = req.json();
+    console.log("res", res);
+  };
+  onTopStories();
+  return <div className="App"></div>;
 }
 
 export default App;
