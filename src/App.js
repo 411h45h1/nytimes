1f3f826d4266cf41e64c63cac39a8a2fe365e8df
Ahmed <ahmedali.96@hotmail.com>
2020-08-22 22:23:47 -0400

    svg logo added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./App.css";
 import AppState from "./context/AppState";
+import { Grid, Segment } from "semantic-ui-react";
+import Logo from "./assets/Logo";
 
-function App() {
+const App = () => {
   return (
     <AppState>
-      <div className="App"></div>
+      <div className="App">
+        <Grid columns={1}>
+          <Grid.Row stretched>
+            <Grid.Column>
+              <Segment className="Body" inverted color="grey">
+                <Logo width="500" height="70" />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     </AppState>
   );
-}
+};
 
 export default App;
