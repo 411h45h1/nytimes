dc789cfe97e6eb28ff39c7052715693953e9ced9
Ahmed <ahmedali.96@hotmail.com>
2020-08-23 00:58:49 -0400

    updated func

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import React from "react";
 import "./App.css";
 import AppState from "./context/AppState";
 import { Grid, Segment } from "semantic-ui-react";
-import Logo from "./assets/Logo";
+import { MediaContextProvider } from "./config/media";
 
+import Logo from "./assets/Logo";
 import Articles from "./components/Articles";
-import ArticleSwitch from "./components/ArticleSwitch";
 
 const App = () => {
   return (
     <AppState>
-      <div className="App">
-        <Grid columns={1}>
-          <Grid.Row stretched>
-            <Grid.Column>
-              <Segment className="Body" inverted color="grey">
-                <Logo width="500" height="70" style={{ marginBottom: 15 }} />
-                <ArticleSwitch />
-                <Articles />
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
+      <MediaContextProvider>
+        <div className="App">
+          <Grid columns={1}>
+            <Grid.Row>
+              <Grid.Column>
+                <Segment className="Body" inverted color="grey">
+                  <Logo width="500" height="70" style={{ marginBottom: 15 }} />
+                  <Articles />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </MediaContextProvider>
     </AppState>
   );
 };
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,16 +1,115 @@
-import React from "react";
-import { Grid, Segment } from "semantic-ui-react";
+import React, { useContext, useState } from "react";
+import { Grid, Segment, Header, Label, Card, Button } from "semantic-ui-react";
+import AppContext from "../context/appContext";
+import { Media } from "../config/media";
 
 const Articles = () => {
-  return (
-    <Grid>
-      <Grid.Row centered columns={1}>
-        <Grid.Column>
-          <Segment inverted style={{ marginBottom: 10 }}>
-            <Segment>a</Segment>
+  const state = useContext(AppContext);
+  const { mostViewedArticles } = state;
+
+  const [timesSelection, setTimesSelection] = useState("viewed");
+
+  const RenderedArticles = () =>
+    mostViewedArticles &&
+    mostViewedArticles.map((article, k) => {
+      return (
+        <Grid.Column key={k}>
+          <Segment style={{ margin: 10 }}>
+            <Header as="h3">
+              {article.title}
+              <Header.Subheader>{article.byline}</Header.Subheader>
+            </Header>
+
+            <Card
+              fluid
+              href={article.url}
+              description={article.abstract}
+              style={{ marginBottom: 30 }}
+            />
+
+            <Label
+              attached="bottom right"
+              color="teal"
+            >{`Published: ${article.published_date}`}</Label>
+
+            {article.subsection && (
+              <Label
+                attached="bottom left"
+                color="grey"
+              >{`Section: ${article.subsection}`}</Label>
+            )}
           </Segment>
         </Grid.Column>
+      );
+    });
+
+  return (
+    <Grid>
+      <Grid.Row columns={3}>
+        <Grid.Column>
+          <Button
+            compact
+            fluid
+            toggle
+            active={timesSelection === "viewed"}
+            content="Most Viewed Articles"
+            onClick={() => setTimesSelection("viewed")}
+          />
+        </Grid.Column>
+        <Grid.Column>
+          <Button
+            compact
+            fluid
+            toggle
+            active={timesSelection === "shared"}
+            content="Most Shared Articles"
+            onClick={() => setTimesSelection("shared")}
+          />
+        </Grid.Column>
+        <Grid.Column>
+          <Button
+            compact
+            fluid
+            toggle
+            active={timesSelection === "emailed"}
+            content="Most Emailed Articles"
+            onClick={() => setTimesSelection("emailed")}
+          />
+        </Grid.Column>
       </Grid.Row>
+      <Segment
+        inverted
+        loading={!mostViewedArticles}
+        style={{
+          minHeight: "20vh",
+          width: "100%",
+          maxHeight: "60vh",
+          overflowY: "scroll",
+        }}
+      >
+        <Grid as={Media} at="mobile">
+          <Grid.Row centered columns={1}>
+            <RenderedArticles />
+          </Grid.Row>
+        </Grid>
+
+        <Grid as={Media} at="tablet">
+          <Grid.Row centered columns={2}>
+            <RenderedArticles />
+          </Grid.Row>
+        </Grid>
+
+        <Grid as={Media} at="computer">
+          <Grid.Row centered columns={3}>
+            <RenderedArticles />
+          </Grid.Row>
+        </Grid>
+        <Grid as={Media} greaterThanOrEqual="largeScreen">
+          <Grid.Row centered columns={4}>
+            <RenderedArticles />
+          </Grid.Row>
+        </Grid>
+      </Segment>
     </Grid>
   );
 };
