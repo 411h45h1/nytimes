9634c4293683caa1bf751e9a8cbc8a0495854d55
Ahmed <ahmedali.96@hotmail.com>
2020-08-23 13:39:58 -0400

    added repo link & responsive logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import AppState from "./context/AppState";
-import { Grid, Segment } from "semantic-ui-react";
+import { Grid, Button, Segment } from "semantic-ui-react";
 import { MediaContextProvider } from "./config/media";
 
 import Logo from "./assets/Logo";
 import Articles from "./components/Articles";
+
+import { Media } from "./config/media";
 
 const App = () => {
   return (
@@ -16,7 +18,39 @@ const App = () => {
             <Grid.Row>
               <Grid.Column>
                 <Segment className="Body" inverted color="grey">
-                  <Logo style={{ marginBottom: 15 }} />
+                  <Grid>
+                    <Grid.Column width={16}>
+                      <Button
+                        compact
+                        size="small"
+                        color="black"
+                        floated="right"
+                        content="Click here for the repository"
+                        as={"a"}
+                        href="https://github.com/AhmedAlihashi/nytimes"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      />
+                    </Grid.Column>
+                  </Grid>
+                  <Media at="mobile">
+                    <Logo style={{ marginTop: 5, marginBottom: 15 }} />
+                  </Media>
+
+                  <Media at="tablet">
+                    <Logo style={{ marginTop: 5, marginBottom: 15 }} />
+                  </Media>
+
+                  <Media at="computer">
+                    <Logo style={{ marginTop: 5, marginBottom: 15 }} />
+                  </Media>
+
+                  <Media greaterThan="computer">
+                    <Logo
+                      style={{ marginTop: 5, marginBottom: 15, height: "20vh" }}
+                    />
+                  </Media>
+
                   <Articles />
                 </Segment>
               </Grid.Column>
